f4a37a81a4bdc91e62fef0914871c963f5235166
ssduman <sduman1818@gmail.com>
2021-07-22 15:25:22 +0300

    update tests

--- spotify_test.go
+++ spotify_test.go
@@ -16,18 +16,21 @@ func TestSliceColumn(t *testing.T) {
 	}
 
 	res, err := sliceColumn(arr, 0)
-	if reflect.DeepEqual(res, []string{"arr0", "arr4", "arr8", "arr12"}) == false || err != nil {
-		t.Errorf("sliceColumn(arr, 0) = %s; want %s", res, []string{"arr0", "arr4", "arr8", "arr12"})
+	correct := []string{"arr0", "arr4", "arr8", "arr12"}
+	if reflect.DeepEqual(res, correct) == false || err != nil {
+		t.Errorf("sliceColumn(arr, 0) = %s; want %s", res, correct)
 	}
 
 	res, err = sliceColumn(arr, 3)
-	if reflect.DeepEqual(res, []string{"arr3", "arr7", "arr11", "arr15"}) == false || err != nil {
-		t.Errorf("sliceColumn(arr, 3) = %s; want %s", res, []string{"arr3", "arr7", "arr11", "arr15"})
+	correct = []string{"arr3", "arr7", "arr11", "arr15"}
+	if reflect.DeepEqual(res, correct) == false || err != nil {
+		t.Errorf("sliceColumn(arr, 3) = %s; want %s", res, correct)
 	}
 
 	res, err = sliceColumn(arr, 4)
-	if reflect.DeepEqual(res, []string{}) != false || err == nil {
-		t.Errorf("sliceColumn(arr, 4) = %s; want %s", res, []string{})
+	correct = []string{}
+	if reflect.DeepEqual(res, correct) != false || err == nil {
+		t.Errorf("sliceColumn(arr, 4) = %s; want %s", res, correct)
 	}
 }
 
@@ -38,39 +41,46 @@ func TestSliceStep(t *testing.T) {
 	}
 
 	res, err := sliceStep(arr, 0, 2)
-	if reflect.DeepEqual(res, []string{"arr0", "arr2", "arr4"}) == false || err != nil {
-		t.Errorf("sliceStep(arr, 0, 2) = %s; want %s", res, []string{"arr0", "arr2", "arr4"})
+	correct := []string{"arr0", "arr2", "arr4"}
+	if reflect.DeepEqual(res, correct) == false || err != nil {
+		t.Errorf("sliceStep(arr, 0, 2) = %s; want %s", res, correct)
 	}
 
 	res, err = sliceStep(arr, 1, 1)
-	if reflect.DeepEqual(res, []string{"arr1", "arr2", "arr3", "arr4", "arr5"}) == false || err != nil {
-		t.Errorf("sliceStep(arr, 1, 1) = %s; want %s", res, []string{"arr1", "arr2", "arr3", "arr4", "arr5"})
+	correct = []string{"arr1", "arr2", "arr3", "arr4", "arr5"}
+	if reflect.DeepEqual(res, correct) == false || err != nil {
+		t.Errorf("sliceStep(arr, 1, 1) = %s; want %s", res, correct)
 	}
 
 	res, err = sliceStep(arr, 6, 2)
-	if reflect.DeepEqual(res, []string{}) != false || err == nil {
-		t.Errorf("sliceStep(arr, 6, 2 = %s; want %s", res, []string{})
+	correct = []string{}
+	if reflect.DeepEqual(res, correct) != false || err == nil {
+		t.Errorf("sliceStep(arr, 6, 2) = %s; want %s", res, correct)
 	}
 }
 
 func TestMs_to_hour(t *testing.T) {
 	res := ms_to_hour(192715, false)
-	if res != "03:12" {
-		t.Errorf("Testms_to_hour(192715, false) = %s; want 03:12", res)
+	correct := "03:12"
+	if res != correct {
+		t.Errorf("ms_to_hour(192715, false) = %s; want %s", res, correct)
 	}
 
 	res = ms_to_hour(192715, true)
-	if res != "00:03:12" {
-		t.Errorf("Testms_to_hour(192715, false) = %s; want 00:03:12", res)
+	correct = "00:03:12"
+	if res != correct {
+		t.Errorf("ms_to_hour(192715, true) = %s; want %s", res, correct)
 	}
 
 	res = ms_to_hour(8627150, false)
-	if res != "23:47" {
-		t.Errorf("Testms_to_hour(8627150, false) = %s; want 23:47", res)
+	correct = "23:47"
+	if res != correct {
+		t.Errorf("ms_to_hour(8627150, false) = %s; want %s", res, correct)
 	}
 
 	res = ms_to_hour(8627150, true)
-	if res != "02:23:47" {
-		t.Errorf("Testms_to_hour(8627150, false) = %s; want 02:23:47", res)
+	correct = "02:23:47"
+	if res != correct {
+		t.Errorf("ms_to_hour(8627150, true) = %s; want %s", res, correct)
 	}
 }
